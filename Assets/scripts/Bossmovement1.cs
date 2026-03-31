7c5ae813d5cfa8dc6e85f18e42aa43a6d1b37434
marim18 <marim18@uia.no>
2026-03-31 13:33:50 +0200

    First draft for boss
    I thiink this restores my work

---
+++ Assets/scripts/Bossmovement1.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Bossmovement1 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+        attack delay function
+        position target function
+        spit projectile function
+        attack mele function.
+        health variable
+        damage variable
+        walk animation?
+
+        range variable
+
+        if health <= 0
+        {
+            die function;
+        }
+        
+        something
+        else
+        {
+            keep playing idle animation; 
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
