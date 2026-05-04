615be7c79de6deac4a225f6492716aa0c3ad5f54
maria <marim18@uia.no>
2026-05-04 12:29:42 +0200

    wip character movement

---
+++ Assets/scripts/InputReader.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class InputReader : MonoBehaviour
+{
+    public Vector3 moveinput { get; private set; }
+    public Vector3 lookinput { get; private set; }
+    public bool attackpressed { get; private set; }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
