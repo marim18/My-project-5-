0644df6344ea4e703859d803d699b3a7ce243b27
maria <marim18@uia.no>
2026-05-04 12:50:25 +0200

    finished inputreader

--- Assets/scripts/InputReader.cs
+++ Assets/scripts/InputReader.cs
@@ -1,21 +1,57 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class InputReader : MonoBehaviour
 {
-<<<<<<< Updated upstream
+
     public Vector3 moveinput { get; private set; }
     public Vector3 lookinput { get; private set; }
     public bool attackpressed { get; private set; }
 
-=======
->>>>>>> Stashed changes
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private PlayerControls controls;
+
+    private void Awake()
+    {
+        controls = new PlayerControls();
+    }
+    private void OnEnable()
+    {
+        controls.Player.Enable();
+        controls.Player.Move.performed += OnMove;
+        controls.Player.Move.canceled += OnMove;
+        controls.Player.Look.performed += OnLook;
+        controls.Player.Look.canceled += OnLook;
+        controls.Player.Attack.performed += OnAttack;
+        controls.Player.Attack.canceled += OnAttack;
+    }
+    private void Ondisable()
+    {
+        controls.Player.Disable();
+        controls.Player.Move.performed -= OnMove;
+        controls.Player.Move.canceled -= OnMove;
+        controls.Player.Look.performed -= OnLook;
+        controls.Player.Look.canceled -= OnLook;
+        controls.Player.Attack.performed -= OnAttack;
+        controls.Player.Attack.canceled -= OnAttack;    
+    }
+    public void OnMove(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        moveinput = context.ReadValue<Vector3>();
+    }
+    public void OnLook(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        lookinput = context.ReadValue<Vector3>();
+    }   
+    public void OnAttack(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        attackpressed = context.ReadValueAsButton();
+    }
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+ 
     void Update()
     {
         
