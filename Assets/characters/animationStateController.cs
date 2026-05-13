c9d186795856a68caedfaa5dea949fef8269b33b
Safa Shahin <safashahin02@icloud.com>
2026-05-13 17:06:55 +0200

    commit
    update

---
+++ Assets/characters/animationStateController.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class animationStateController : MonoBehaviour
+{
+    Animator animator;
+
+    int isWalkingHash;
+    int isRunningHash;
+    int jumpHash;
+    int swordAttackHash;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        isWalkingHash = Animator.StringToHash("IsWalking");
+        isRunningHash = Animator.StringToHash("IsRunning");
+        jumpHash = Animator.StringToHash("Jump");
+        swordAttackHash = Animator.StringToHash("SwordAttack");
+    }
+
+    void Update()
+    {
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool isRunning = animator.GetBool(isRunningHash);
+
+        bool forwardPressed = Input.GetKey(KeyCode.W);
+        bool runPressed = Input.GetKey(KeyCode.LeftShift);
+
+        bool jumpPressed = Input.GetKeyDown(KeyCode.Space);
+        bool swordPressed = Input.GetKeyDown(KeyCode.F);
+
+        // Walking
+        if (!isWalking && forwardPressed)
+        {
+            animator.SetBool(isWalkingHash, true);
+        }
+
+        if (isWalking && !forwardPressed)
+        {
+            animator.SetBool(isWalkingHash, false);
+        }
+
+        // Running
+        if (!isRunning && forwardPressed && runPressed)
+        {
+            animator.SetBool(isRunningHash, true);
+        }
+
+        if (isRunning && (!forwardPressed || !runPressed))
+        {
+            animator.SetBool(isRunningHash, false);
+        }
+
+        // Jump
+        if (jumpPressed)
+        {
+            animator.SetTrigger(jumpHash);
+        }
+
+        // Sword attack
+        if (swordPressed)
+        {
+            animator.SetTrigger(swordAttackHash);
+        }
+    }
+}
