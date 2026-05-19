c5bfcf649af7202dff4b515bc81a7024baf25562
Safa Shahin <safashahin02@icloud.com>
2026-05-19 12:00:54 +0200

    particle/sparkle effect for UX
    swordscript.cs:
    
    This part of the script creates th small sparkle/collection effect automatically with:
    
    GameObject effectObject = new GameObject("SwordPiecePickupEffect");
    ParticleSystem particleSystem = effectObject.AddComponent<ParticleSystem>();

--- Assets/scripts/ui connector scripts/swordscript.cs
+++ Assets/scripts/ui connector scripts/swordscript.cs
@@ -32,10 +32,48 @@ public class swordscript : MonoBehaviour
 
             Debug.Log("Player picked up sword piece with ID: " + Swordpieceid);
 
+            // Create sparkle effect at the position of the collected sword piece
+            CreatePickupEffect();
+
+            // Update progress bar, inventory, and sword upgrade logic
             swordProgressManager.CollectSwordPiece(Swordpieceid);
 
             // Hide/remove the collected sword piece from the scene
             gameObject.SetActive(false);
         }
+    }
+
+    void CreatePickupEffect()
+    {
+        GameObject effectObject = new GameObject("SwordPiecePickupEffect");
+        effectObject.transform.position = transform.position;
+
+        ParticleSystem particleSystem = effectObject.AddComponent<ParticleSystem>();
+
+        var main = particleSystem.main;
+        main.duration = 0.6f;
+        main.startLifetime = 0.5f;
+        main.startSpeed = 2.5f;
+        main.startSize = 0.15f;
+        main.maxParticles = 30;
+        main.loop = false;
+
+        var emission = particleSystem.emission;
+        emission.rateOverTime = 0;
+        emission.SetBursts(new ParticleSystem.Burst[]
+        {
+            new ParticleSystem.Burst(0f, 25)
+        });
+
+        var shape = particleSystem.shape;
+        shape.shapeType = ParticleSystemShapeType.Sphere;
+        shape.radius = 0.4f;
+
+        var renderer = particleSystem.GetComponent<ParticleSystemRenderer>();
+        renderer.renderMode = ParticleSystemRenderMode.Billboard;
+
+        particleSystem.Play();
+
+        Destroy(effectObject, 1.2f);
     }
 }
