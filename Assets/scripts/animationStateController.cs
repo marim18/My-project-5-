6bf99b5cadbdef92fa03c1a255bc3500d59287d7
marim18 <marim18@uia.no>
2026-05-22 11:04:39 +0200

    jump

--- Assets/scripts/animationStateController.cs
+++ Assets/scripts/animationStateController.cs
@@ -1,24 +1,30 @@
 using UnityEngine;
 
-public class animationStateController : MonoBehaviour
+public class AnimationStateController : MonoBehaviour
 {
-    public Animator animator;
+   public Animator animator;
 
-    int isWalkingHash;
+   int isWalkingHash;
     int isRunningHash;
     int jumpHash;
     int swordAttackHash;
-
-    public bool isWalking;
-    public bool isRunning;
+    public bool isWalking =false;
+    public bool isRunning = false;
+    float inputaxisy;
+    public float inputaxisx;
+        public float jumpForce = 7f;
+   
 
     public float walkSpeed = 1.5f;
     public float runSpeed = 4f;
 
+    public Rigidbody rb;
+
+       
     void Start()
     {
+       Debug.Log("Animation State Controller started successfully."); 
         animator = GetComponent<Animator>();
-
         if (animator == null)
         {
             Debug.LogError("No Animator component found on the character!");
@@ -27,68 +33,79 @@ public class animationStateController : MonoBehaviour
         isWalkingHash = Animator.StringToHash("IsWalking");
         isRunningHash = Animator.StringToHash("IsRunning");
         jumpHash = Animator.StringToHash("Jump");
-        swordAttackHash = Animator.StringToHash("SwordAttack");
+        swordAttackHash = Animator.StringToHash("SwordAttack"); 
+       
     }
 
     void Update()
-    {
+    {  
+        Debug.Log("Update method called in Animation State Controller.");
         isWalking = animator.GetBool(isWalkingHash);
         isRunning = animator.GetBool(isRunningHash);
 
+        inputaxisy = Input.GetAxis("Vertical");   
+        inputaxisx= Input.GetAxis("Horizontal");
+        bool keyboardinput = inputaxisy != 0 || inputaxisx != 0;
+       
         bool runPressed = Input.GetKey(KeyCode.LeftShift);
+
         bool jumpPressed = Input.GetKeyDown(KeyCode.Space);
         bool swordPressed = Input.GetKeyDown(KeyCode.F);
 
-        float horizontal = 0f;
-        float vertical = 0f;
-
-       // Forward / backward
-if (Input.GetKey(KeyCode.W))
-{
-    vertical = 1f;
-}
-else if (Input.GetKey(KeyCode.S))
-{
-    vertical = -1f;
-}
-
-// Left / right
-if (Input.GetKey(KeyCode.D))
-{
-    horizontal = -1f;
-}
-else if (Input.GetKey(KeyCode.A))
-{
-    horizontal = 1f;
-}
-
-        Vector3 moveDirection = new Vector3(horizontal, 0f, vertical).normalized;
-
-        bool isMoving = moveDirection.magnitude > 0;
 
         // Walking animation
-        animator.SetBool(isWalkingHash, isMoving);
+        if (!isWalking && keyboardinput && !runPressed)
+        {
+            animator.SetBool(isWalkingHash, true);
+            Debug.Log("Walking animation triggered.");
+        }
+
+        if (isWalking && !keyboardinput)
+        {
+            animator.SetBool(isWalkingHash, false);
+        }
 
         // Running animation
-        animator.SetBool(isRunningHash, isMoving && runPressed);
+        if (!isRunning && keyboardinput && runPressed)
+        {
+            animator.SetBool(isRunningHash, true);
+        }
+
+        if (isRunning && (!keyboardinput || !runPressed))
+        {
+            animator.SetBool(isRunningHash, false);
+        }
+        
 
         // Jump animation
         if (jumpPressed)
         {
+                 rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
+                 Debug.Log(rb.linearVelocity + "jump speed"); 
             animator.SetTrigger(jumpHash);
+            
         }
 
-        // Sword attack animation
+        // Sword animation
         if (swordPressed)
         {
             animator.SetTrigger(swordAttackHash);
         }
 
-        // Actual movement
-        if (isMoving)
+        // Actual character movement
+        if (keyboardinput)
         {
             float currentSpeed = runPressed ? runSpeed : walkSpeed;
+           
 
-            transform.Translate(moveDirection * currentSpeed * Time.deltaTime, Space.Self);     }
+            Vector3 movement = new Vector3(-inputaxisx, 0, -inputaxisy).normalized * currentSpeed * Time.deltaTime;
+            Quaternion targetRotation = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 0.1f);
+            transform.position += movement;
+
+        }
+
+       
     }
+    
 }
